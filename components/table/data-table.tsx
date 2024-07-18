09952f10536286c618b5bab948e67f2f156a54fc
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2024-07-18 05:37:45 +0300

    feat(table): added filtering functionality using @tanstack

--- components/table/data-table.tsx
+++ components/table/data-table.tsx
@@ -9,11 +9,14 @@ import {
   SortingState,
   getSortedRowModel,
   useReactTable,
+  ColumnFiltersState,
+  getFilteredRowModel,
 } from "@tanstack/react-table";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 import { useEffect } from "react";
 
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
   Table,
@@ -24,14 +27,40 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { decryptKey } from "@/lib/utils";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
 }
 
+type InputFilter = "primaryPhysician" | "status" | "patient" | "schedule" | string;
+
+const getFilterName = (filter: InputFilter) => {
+  switch (filter) {
+    case "primaryPhysician":
+      return "Doctor";
+    case "status":
+      return "Status";
+    case "patient":
+      return "Patient";
+    case "schedule":
+      return "Appointment";
+  }
+};
+
 export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
+  const [inputFilter, setInputFilter] = React.useState<InputFilter>("primaryPhysician");
 
   const encryptedKey =
     typeof window !== "undefined" ? window.localStorage.getItem("accessKey") : null;
@@ -51,13 +80,40 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
     getPaginationRowModel: getPaginationRowModel(),
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
     state: {
       sorting,
+      columnFilters,
     },
   });
 
   return (
     <div className="data-table">
+      <div className="m-4 flex items-center justify-center">
+        <Input
+          placeholder={`Filter by ${getFilterName(inputFilter)}...`}
+          value={(table.getColumn(inputFilter)?.getFilterValue() as string) ?? ""}
+          onChange={event => table.getColumn(inputFilter)?.setFilterValue(event.target.value)}
+          className="text-14-medium rounded-lg border border-dark-500"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">{getFilterName(inputFilter)}</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="shad-dialog">
+            <DropdownMenuLabel>Available Filters</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuRadioGroup value={inputFilter} onValueChange={setInputFilter}>
+              <DropdownMenuRadioItem value="primaryPhysician">Doctor</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="status">Status</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="patient">Patient</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="schedule">Schedule</DropdownMenuRadioItem>
+            </DropdownMenuRadioGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+
       <Table className="shad-table">
         <TableHeader className="bg-dark-200">
           {table.getHeaderGroups().map(headerGroup => (
